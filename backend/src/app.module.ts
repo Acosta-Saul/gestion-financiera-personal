75dbb3cf60234f5e1abe5384d3e7075c8af8dafd
Saúl David Acosta Benavides <srmixter123@gmail.com>
2025-06-29 21:13:22 -0400

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,11 +8,11 @@ import { AsientoModule } from './Asiento/asiento.module';
   imports: [
   TypeOrmModule.forRoot({
       type: 'postgres',
-      host: dpg-d1grp2jipnbc73b4d2mg-a.oregon-postgres.render.com,
+      host: 'dpg-d1grp2jipnbc73b4d2mg-a.oregon-postgres.render.com',
       port: 5432,
-      username: finanza_z1pf_user,
-      password: Yi5vcUTP34RziAbwuUWEEHsCZIamh513,
-      database: finanza_z1pf,
+      username: 'finanza_z1pf_user',
+      password: 'Yi5vcUTP34RziAbwuUWEEHsCZIamh513',
+      database: 'finanza_z1pf',
       synchronize: true,
       ssl:
         process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
