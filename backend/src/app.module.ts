22feb64236cd86fc98e3ee345ed2f91298240770
Saúl David Acosta Benavides <srmixter123@gmail.com>
2025-06-30 08:55:33 -0400

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,7 +8,11 @@ import { AsientoModule } from './Asiento/asiento.module';
   imports: [
   TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
       ssl: { rejectUnauthorized: false },
     }),
